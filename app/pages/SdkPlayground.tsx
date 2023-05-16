65979b3422e8669e870935e2f7776ecd68fa09db
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-05-16 17:02:23 +0200

    setup monaco editor

--- app/pages/SdkPlayground.tsx
+++ app/pages/SdkPlayground.tsx
@@ -1,7 +1,42 @@
+import { EditorProps } from "@monaco-editor/react"
 import type { NextPage } from "next"
+import dynamic from "next/dynamic"
+
+const MonacoEditor = dynamic<EditorProps>(
+  () => import("@monaco-editor/react") as any,
+  {
+    ssr: false,
+  }
+)
 
 const SdkPlayground: NextPage = () => {
-  return <p> TODO</p>
+  return (
+    <>
+      <div className="monaco-wrapper">
+        <MonacoEditor
+          value="default"
+          width="100vw"
+          height="calc(100vh - 64px - 140px)"
+          language="typescript"
+          options={{ minimap: { enabled: false } }}
+          onChange={(newValue) => {
+            console.log({ newValue })
+          }}
+        />
+      </div>
+      <style jsx global>{`
+        .nextra-content {
+          min-height: 0 !important;
+          height: calc(100vh - 64px - 140px) !important;
+        }
+        .monaco-wrapper {
+          position: fixed;
+          left: 0;
+          height: calc(100vh - 64px - 140px);
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default SdkPlayground
