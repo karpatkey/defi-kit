2ec68f1ec02d1b77892a73ee7c0bd4ad828ebb75
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-05-30 13:14:11 +0200

    add some recipes

--- sdk/src/protocols/schema.ts
+++ sdk/src/protocols/schema.ts
@@ -1,12 +1,13 @@
 import * as curve from "./curve/schema"
 import * as cowswap from "./cowswap/schema"
+import { ProtocolSchemas } from "../types"
 
 // group all protocols schemas by chain
 
 export const eth = {
   curve: curve.eth,
-}
+} satisfies Record<string, ProtocolSchemas>
 
 export const gor = {
   cowswap: cowswap.gor,
-}
+} satisfies Record<string, ProtocolSchemas>
--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -1,3 +1,4 @@
+import { SomeZodObject } from "zod"
 import { PresetAllowEntry } from "zodiac-roles-sdk/index"
 
 export type Action = "deposit" | "borrow" | "stake" | "claim" | "swap"
@@ -22,4 +23,13 @@ export type ProtocolActions = {
     buy?: any[]
     pools?: any[]
   }) => PresetAllowEntry[]
+}
+
+// For registering protocols in the REST API we need zod schemas for the specific parameters of each action
+export type ProtocolSchemas = {
+  deposit?: SomeZodObject
+  borrow?: SomeZodObject
+  stake?: SomeZodObject
+  claim?: SomeZodObject
+  swap?: SomeZodObject
 }
