414f5063e448ff7744ffd1cbb47c5b0fcea9bb71
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-06-22 15:22:37 +0200

    allow deposits to aave

--- sdk/eth-sdk/config.ts
+++ sdk/eth-sdk/config.ts
@@ -2,14 +2,14 @@ import { EthSdkConfig, defineConfig } from "@dethcrypto/eth-sdk"
 
 export const contracts = {
   mainnet: {
-    aave_v2: {
-      AaveLendingPoolV2: "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9",
-      ParaSwapRepayAdapter: "0x80Aca0C645fEdABaa20fd2Bf0Daf57885A309FE6",
-      WrappedTokenGatewayV2: "0xEFFC18fC3b7eb8E676dac549E0c693ad50D1Ce31",
-      AAVE: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
-      ABPT: "0x41A08648C3766F9F9d85598fF102a08f4ef84F84",
-      stkAAVE: "0x4da27a545c0c5b758a6ba100e3a049001de870f5",
-      stkABPT: "0xa1116930326D21fB917d5A27F1E9943A9595fb47",
+    aaveV2: {
+      aaveLendingPoolV2: "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9",
+      paraSwapRepayAdapter: "0x80Aca0C645fEdABaa20fd2Bf0Daf57885A309FE6",
+      wrappedTokenGatewayV2: "0xEFFC18fC3b7eb8E676dac549E0c693ad50D1Ce31",
+      aave: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+      abpt: "0x41A08648C3766F9F9d85598fF102a08f4ef84F84",
+      stkaave: "0x4da27a545c0c5b758a6ba100e3a049001de870f5",
+      stkabpt: "0xa1116930326D21fB917d5A27F1E9943A9595fb47",
     },
     curve: {
       regularPool: "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
