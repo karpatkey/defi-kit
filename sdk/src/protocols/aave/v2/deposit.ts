2c1a494ee495ea86947af2779734a3ed2e11b221
nledo <nicolas@karpatkey.com>
2023-06-23 10:50:23 -0300

    Aave progress

--- sdk/src/protocols/aave/v2/deposit.ts
+++ sdk/src/protocols/aave/v2/deposit.ts
@@ -4,6 +4,8 @@ import { Token } from "./types"
 import { allowErc20Approve } from "../../../erc20"
 import { contracts } from "../../../../eth-sdk/config"
 
+const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
 export const depositToken = (token: Token) => {
   return [
     allowErc20Approve(
@@ -16,6 +18,14 @@ export const depositToken = (token: Token) => {
       AVATAR,
       "0x"
     ),
+    allow.mainnet.aaveV2.aaveLendingPoolV2.withdraw(
+      token.token,
+      undefined,
+      AVATAR
+    ),
+    allow.mainnet.aaveV2.aaveLendingPoolV2.setUserUseReserveAsCollateral(
+      token.token
+    )
   ]
 }
 
@@ -26,4 +36,12 @@ export const depositEther = () => [
     "0x",
     { send: true }
   ),
+  allow.mainnet.aaveV2.wrappedTokenGatewayV2.withdrawETH(
+    contracts.mainnet.aaveV2.aaveLendingPoolV2,
+    undefined,
+    AVATAR
+  ),
+  allow.mainnet.aaveV2.aaveLendingPoolV2.setUserUseReserveAsCollateral(
+    WETH
+  )
 ]
