8baa255f041f7a618293b56fceead0460bc3cd19
nledo <nicolas@karpatkey.com>
2023-06-27 13:16:09 -0300

    CompoundV3 _comets.ts complete

--- sdk/src/protocols/compound/v3/_comets.ts
+++ sdk/src/protocols/compound/v3/_comets.ts
@@ -1,10 +1,11 @@
 export default [
     {
-        symbol: 'cUSDCv3',
-        address: '0x...', // USDC comet address
-        borrowToken: [
-            { symbol: 'USDC', address: '0x'}
-        ],
+        address: "0xc3d688B66703497DAA19211EEdff47f25384cdc3",
+        symbol: "cUSDCv3",
+        borrowToken: {
+            address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            symbol: "USDC"
+        },
         collateralTokens: [
             {
                 address: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
@@ -28,9 +29,23 @@ export default [
             }
         ]
     },
-//    {
-//     name: 'cETHv3',
-//     address: '0x...',
-//    }
+    {
+        address: "0xA17581A9E3356d9A858b789D68B4d866e593aE94",
+        symbol: "cWETHv3",
+        borrowToken: {
+            address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+            symbol: "ETH"
+        },
+        collateralTokens: [
+            {
+                address: "0xBe9895146f7AF43049ca1c1AE358B0541Ea49704",
+                symbol: "cbETH"
+            },
+            {
+                address: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+                symbol: "wstETH"
+            }
+        ]
+    }
 ] as const
 
