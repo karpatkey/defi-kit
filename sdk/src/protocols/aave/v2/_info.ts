7fcc2100e53fae4e3cfaa2e4016bee2338c75918
nledo <nledo@users.noreply.github.com>
2023-06-30 01:55:47 +0000

    protocol updates

--- sdk/src/protocols/aave/v2/_info.ts
+++ sdk/src/protocols/aave/v2/_info.ts
@@ -28,6 +28,7 @@ export default [
   {
     symbol: "WETH",
     token: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    usageAsCollateralEnabled: true,
     borrowingEnabled: true,
     stableBorrowRateEnabled: true,
     isActive: true,
