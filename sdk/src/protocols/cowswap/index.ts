5f0f3494c9cdda92c7254d32c6b3dde52b081d2d
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-07-04 19:55:56 +0200

    fix typing issue

--- sdk/src/protocols/cowswap/index.ts
+++ sdk/src/protocols/cowswap/index.ts
@@ -1,14 +1,14 @@
 import { c } from "zodiac-roles-sdk"
 import { allow } from "zodiac-roles-sdk/kit"
 
-const tokenListScoping = (tokens: string[] | undefined) =>
+const tokenListScoping = (tokens?: string[]) =>
   !tokens || tokens.length === 0
     ? undefined
     : tokens.length === 1
     ? tokens[0]
     : c.or(...(tokens as [string, string, ...string[]]))
 
-const swap = (options: { sell: string[]; buy: string[] }) => {
+const swap = (options: { sell?: string[]; buy?: string[] }) => {
   const sellScoping = tokenListScoping(options.sell)
   const buyScoping = tokenListScoping(options.buy)
   return [allow.goerli.cowswap.orderSigner.signOrder(sellScoping, buyScoping)]
