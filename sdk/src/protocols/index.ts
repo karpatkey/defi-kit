e9acb3f88839f8a2625eb8d1817c2581b4534112
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-07-11 12:08:01 +0200

    aave and compound exports

--- sdk/src/protocols/index.ts
+++ sdk/src/protocols/index.ts
@@ -1,6 +1,10 @@
 import { ProtocolActions } from "../types"
+import * as aave_v2 from "./aave/v2"
+import * as aave_v3 from "./aave/v3"
 import * as aura from "./aura"
 import * as balancer from "./balancer"
+import * as compound_v2 from "./compound/v2"
+import * as compound_v3 from "./compound/v3"
 import * as curve from "./curve"
 import * as cowswap from "./cowswap"
 import * as lido from "./lido"
@@ -8,8 +12,12 @@ import * as lido from "./lido"
 // group all protocols actions by chain
 
 export const eth = {
+  aave_v2: aave_v2.eth,
+  aave_v3: aave_v3.eth,
   aura: aura.eth,
   balancer: balancer.eth,
+  compound_v2: compound_v2.eth,
+  compound_v3: compound_v3.eth,
   curve: curve.eth,
   lido: lido.eth,
 } satisfies Record<string, ProtocolActions>
