9110e5c61cc39cf198d1614e652c7ad7b5143c0e
nledo <nicolas@karpatkey.com>
2023-12-06 17:52:48 -0300

    Maker info script

---
+++ scripts/maker.py
@@ -0,0 +1,37 @@
+from defyes.functions import get_contract, get_symbol
+from defyes.node import get_node
+from defyes.constants import Chain
+from lib.dump import dump
+import requests
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ABIs
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Gem Join - gem, ilk
+ABI_GEM_JOIN = '[{"constant":true,"inputs":[],"name":"gem","outputs":[{"internalType":"contract GemLike","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"ilk","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}]'
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# transactions_data
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def transactions_data():
+    chainlog_data = requests.get("https://chainlog.makerdao.com/api/mainnet/active.json").json()
+
+    web3 = get_node(Chain.ETHEREUM)
+    result = []
+    for item in chainlog_data:
+        if 'MCD_JOIN' in item and item not in ['MCD_JOIN_DAI', 'MCD_JOIN_TELEPORT_FW_A']:
+            gem_join_contract = get_contract(chainlog_data[item], Chain.ETHEREUM, web3=web3, abi=ABI_GEM_JOIN)
+            gem_address = gem_join_contract.functions.gem().call()
+            result.append(
+                {
+                    'address': gem_address,
+                    'symbol': get_symbol(gem_address, Chain.ETHEREUM, web3=web3),
+                    'ilkDescription': item[9:len(item)],
+                    'ilk': '0x' + gem_join_contract.functions.ilk().call().hex()
+                }
+            )
+    
+    dump(result, 'maker')
+
+transactions_data()
