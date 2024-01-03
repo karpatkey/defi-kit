69dcb8f5f0c1154d475f1b98a42f6aeca479b629
nledo <nicolas@karpatkey.com>
2024-01-03 16:45:07 -0300

    Fixed lido test

--- sdk/src/protocols/lido/deposit.test.ts
+++ sdk/src/protocols/lido/deposit.test.ts
@@ -5,8 +5,16 @@ import { contracts } from "../../../eth-sdk/config"
 import { Status } from "../../../test/types"
 import { testKit } from "../../../test/kit"
 import { parseEther } from "ethers/lib/utils"
+import { getMainnetSdk } from "@dethcrypto/eth-sdk-client"
 
-const steCRV_pool = "0xDC24316b9AE028F1497c275EB9192a3Ea0f67022"
+
+async function delay(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+const sdk = getMainnetSdk(avatar)
 
 describe("lido", () => {
   describe("deposit", () => {
@@ -17,32 +25,49 @@ describe("lido", () => {
     it("allows submitting ETH", async () => {
       await expect(
         testKit.eth.lido.steth.submit(avatar._address, {
-          value: parseEther('2'),
+          value: parseEther('10'),
         })
       ).not.toRevert()
     })
 
     it("allows wrapping and unwrapping stETH", async () => {
-      await stealErc20(contracts.mainnet.lido.steth, parseEther('2'), steCRV_pool)
       await expect(
-        testKit.eth.lido.steth.approve(contracts.mainnet.lido.wsteth, parseEther('2'))
+        testKit.eth.lido.steth.approve(contracts.mainnet.lido.wsteth, parseEther('5'))
       ).not.toRevert()
-      await expect(testKit.eth.lido.wsteth.wrap(parseEther('2'))).not.toRevert()
+      await expect(testKit.eth.lido.wsteth.wrap(parseEther('5'))).not.toRevert()
       await expect(testKit.eth.lido.wsteth.unwrap(parseEther('1'))).not.toRevert()
     })
 
     it("only allows requesting withdrawals from avatar's positions", async () => {
-      const avatarAddress = avatar._address
-      await expect(
-        testKit.eth.lido.steth.approve(contracts.mainnet.lido.unsteth, parseEther('2'))
+      var amount = await sdk.lido.steth.balanceOf(
+        avatar._address
       )
+      console.log("Amount of stETH: ", amount.toBigInt())
       await expect(
-        testKit.eth.lido.unsteth.requestWithdrawals([parseEther('1')], avatarAddress)
+        testKit.eth.lido.steth.approve(contracts.mainnet.lido.unsteth, parseEther('1'))
+      )
+      await delay(2000);
+      await expect(
+        testKit.eth.lido.unsteth.requestWithdrawals([parseEther('1')], avatar._address)
       ).not.toRevert()
 
-      const anotherAddress = member._address
+      var amount = await sdk.lido.wsteth.balanceOf(
+        avatar._address
+      )
+      console.log("Amount of wstETH: ", amount.toBigInt())
       await expect(
-        testKit.eth.lido.unsteth.requestWithdrawals([parseEther('1')], anotherAddress)
+        testKit.eth.lido.wsteth.approve(contracts.mainnet.lido.unsteth, parseEther('1'))
+      )
+      await delay(2000);
+      await expect(
+        testKit.eth.lido.unsteth.requestWithdrawalsWstETH([parseEther('1')], avatar._address)
+      ).not.toRevert()
+
+      await expect(
+        testKit.eth.lido.unsteth.requestWithdrawals([parseEther('1')], member._address)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+      await expect(
+        testKit.eth.lido.unsteth.requestWithdrawalsWstETH([parseEther('1')], member._address)
       ).toBeForbidden(Status.ParameterNotAllowed)
     })
   })
