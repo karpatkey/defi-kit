cfa8313c386960a55c54c16456a9e7a6106367aa
nledo <nicolas@karpatkey.com>
2024-01-18 17:53:33 -0300

    Finished aavev2 docs / Updated delegate action and tests

--- sdk/src/protocols/aave/v2/delegate.test.ts
+++ sdk/src/protocols/aave/v2/delegate.test.ts
@@ -1,10 +1,8 @@
 import { eth } from "."
-import { avatar, member } from "../../../../test/wallets"
+import { member } from "../../../../test/wallets"
 import { applyPermissions } from "../../../../test/helpers"
-import { contracts } from "../../../../eth-sdk/config"
 import { Status } from "../../../../test/types"
 import { testKit } from "../../../../test/kit"
-import { parseEther } from "ethers/lib/utils"
 
 const DELEGATEE = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
 
@@ -30,11 +28,6 @@ describe("aave_v2", () => {
       await expect(
         testKit.eth.aaveV2.aave.delegateByType(member._address, 0)
       ).toBeForbidden(Status.ParameterNotAllowed)
-
-      // Test to go through roles
-      await expect(
-        testKit.eth.aaveV2.governanceV2.submitVote(1, true)
-      ).toBeAllowed()
     })
   })
 })
--- sdk/src/protocols/aave/v2/delegateTokens.ts
+++ sdk/src/protocols/aave/v2/delegateTokens.ts
@@ -7,4 +7,8 @@ export default [
     address: "0x4da27a545c0c5b758a6ba100e3a049001de870f5",
     symbol: "stkAAVE",
   },
+  {
+    address: "0xA700b4eB416Be35b2911fd5Dee80678ff64fF6C9",
+    symbol: "aEthAAVE",
+  },
 ] as const
