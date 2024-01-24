527673de49fad7ee56b77fb4e14e6a43a40e4fd6
nledo <nicolas@karpatkey.com>
2024-01-24 17:28:04 -0300

    api-docs new format

--- sdk/test/provider.ts
+++ sdk/test/provider.ts
@@ -7,7 +7,7 @@ export const getProvider = () => {
       url: "http://127.0.0.1:8545",
       // https://docs.ethers.org/v5/api/utils/web/#ConnectionInfo
       // How long to wait (in ms) before rejecting with a timeout error. (default: 120000).
-      timeout: 180000,
+      timeout: 240000,
     })
   }
   return provider
