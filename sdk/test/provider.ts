3ee4620a1a648d83432fbae173b36c3ac327164e
nledo <nicolas@karpatkey.com>
2024-01-23 22:59:49 -0300

    rollback

--- sdk/test/provider.ts
+++ sdk/test/provider.ts
@@ -4,9 +4,6 @@ let provider: providers.JsonRpcProvider
 export const getProvider = () => {
   if (!provider) {
     provider = new providers.JsonRpcProvider("http://127.0.0.1:8545")
-    // How long to wait (in ms) before rejecting with a timeout error. (default: 120000).
-    // https://docs.ethers.org/v5/api/utils/web/#ConnectionInfo
-    provider.connection.timeout = 180000
   }
   return provider
 }
