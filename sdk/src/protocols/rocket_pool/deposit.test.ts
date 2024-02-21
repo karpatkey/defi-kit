ae7ef49846377a126bd4ff80029d526be2ed5db8
nledo <103588782+nledo@users.noreply.github.com>
2024-02-21 09:07:57 -0300

    feat: add rocket pool (#98)

---
+++ sdk/src/protocols/rocket_pool/deposit.test.ts
@@ -0,0 +1,68 @@
+import { eth } from "."
+import { queryDepositPool } from "./utils"
+import { applyPermissions, stealErc20 } from "../../../test/helpers"
+import { getMainnetSdk } from "@dethcrypto/eth-sdk-client"
+import { contracts } from "../../../eth-sdk/config"
+import { testKit } from "../../../test/kit"
+import { parseEther } from "ethers/lib/utils"
+
+const address_with_rETH = "0x1BeE69b7dFFfA4E2d53C2a2Df135C388AD25dCD2"
+
+describe("rocket_pool", () => {
+  describe("deposit", () => {
+    beforeAll(async () => {
+      await applyPermissions(await eth.deposit())
+    })
+
+    it("query pool", async () => {
+      const deposit_pool = await queryDepositPool()
+      console.log("Deposit Pool: ", deposit_pool)
+    })
+
+    it("deposit and withdraw through deposit pool", async () => {
+      await expect(
+        testKit.eth.rocket_pool.deposit_pool.deposit(
+          {
+            value: parseEther("1")
+          }
+        )
+      ).not.toRevert()
+
+      await expect(
+        testKit.eth.rocket_pool.rETH.burn(parseEther("0.01"))
+      ).not.toRevert()
+    })
+
+    it("deposit and withdraw using secondary markets with swap router (only through roles)", async () => {
+      await expect(
+        testKit.eth.rocket_pool.swap_router.swapTo(
+          3,
+          7,
+          parseEther("400"),
+          parseEther("450"),
+          {
+            value: parseEther("500")
+          }
+        )
+      ).toBeAllowed()
+
+      await expect(
+        testKit.eth.rocket_pool.rETH.approve(
+          contracts.mainnet.rocket_pool.swap_router,
+          parseEther("50000"),
+        )
+      ).toBeAllowed()
+
+      console.log("before swapFrom")
+      await expect(
+        testKit.eth.rocket_pool.swap_router.swapFrom(
+          0,
+          10,
+          parseEther("14000"),
+          parseEther("15000"),
+          parseEther("50000"),
+        )
+      ).toBeAllowed()
+    })
+  })
+})
