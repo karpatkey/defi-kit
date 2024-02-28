93a17bf74f4542efc7b30b5626a39fb158703b37
nledo <nicolas@karpatkey.com>
2024-02-28 09:53:51 -0300

    feat: add uniswapv3

---
+++ scripts/uniswap_v3.py
@@ -0,0 +1,152 @@
+import time
+# thegraph queries
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from defyes.functions import get_node
+from karpatkit.constants import Chain
+from lib.dump import dump
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# subgraph_query_all_pools
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def subgraph_query_all_pools(blockchain, min_tvl_usd=0, min_volume_usd=0):
+
+    # Initialize subgraph
+    subgraph_url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+    uniswapv3_transport = RequestsHTTPTransport(
+        url=subgraph_url, verify=True, retries=3
+    )
+    client = Client(transport=uniswapv3_transport)
+
+    pools = {}
+    response = {}
+    last_timestamp = 0
+    all_found = False
+
+    web3 = get_node(blockchain)
+
+    try:
+        query_string = """
+        query {{
+        pools(first: 1000, orderBy: createdAtTimestamp, orderDirection: asc) 
+            {{
+                id
+                token0{{id symbol}}
+                token1{{id symbol}}
+                feeTier
+                volumeUSD
+                totalValueLockedUSD
+                createdAtTimestamp
+            }}
+        }}
+        """
+
+        formatted_query_string = query_string.format()
+        response = client.execute(gql(formatted_query_string))
+
+        for pool in response["pools"]:
+            volume_usd = float(pool["volumeUSD"])
+            tvl_usd = float(pool["totalValueLockedUSD"])
+
+            if volume_usd >= min_volume_usd and tvl_usd >= min_tvl_usd:
+                pools[pool["id"]] = [
+                    web3.to_checksum_address(pool["token0"]["id"]),
+                    pool["token0"]["symbol"],
+                    web3.to_checksum_address(pool["token1"]["id"]),
+                    pool["token1"]["symbol"],
+                    int(pool["feeTier"]),
+                    volume_usd,
+                    tvl_usd,
+                ]
+
+        if len(response["pools"]) < 1000:
+            all_found = True
+        else:
+            last_timestamp = int(pool["createdAtTimestamp"]) - 1
+
+    except Exception as Ex:
+        print(Ex)
+
+    if not all_found and last_timestamp > 0:
+
+        try:
+            while not all_found:
+                query_string = """
+                query {{
+                pools(first: 1000, orderBy: createdAtTimestamp, orderDirection: asc
+                where: {{createdAtTimestamp_gt: {last_timestamp} }})
+                    {{
+                        id
+                        token0{{id symbol}}
+                        token1{{id symbol}}
+                        feeTier
+                        volumeUSD
+                        totalValueLockedUSD
+                        createdAtTimestamp
+                    }}
+                }}
+                """
+
+                formatted_query_string = query_string.format(
+                    last_timestamp=last_timestamp
+                )
+                response = client.execute(gql(formatted_query_string))
+
+                for pool in response["pools"]:
+                    volume_usd = float(pool["volumeUSD"])
+                    tvl_usd = float(pool["totalValueLockedUSD"])
+
+                    if volume_usd >= min_volume_usd and tvl_usd >= min_tvl_usd:
+                        pools[pool["id"]] = [
+                            web3.to_checksum_address(pool["token0"]["id"]),
+                            pool["token0"]["symbol"],
+                            web3.to_checksum_address(pool["token1"]["id"]),
+                            pool["token1"]["symbol"],
+                            int(pool["feeTier"]),
+                            volume_usd,
+                            tvl_usd,
+                        ]
+
+                if len(response["pools"]) < 1000:
+                    all_found = True
+                else:
+                    last_timestamp = int(pool["createdAtTimestamp"]) - 1
+                    time.sleep(5)
+
+        except Exception as Ex:
+            print(Ex)
+
+    return pools
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# protocol_data
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def protocol_data(blockchain, min_tvl_usd=0, min_volume_usd=0):
+
+    if blockchain == Chain.ETHEREUM:
+        tokens = []
+
+    pools = subgraph_query_all_pools(blockchain, min_tvl_usd=min_tvl_usd, min_volume_usd=min_volume_usd)
+
+    if len(pools) > 0:
+        pools = dict(sorted(pools.items(), key=lambda item: item[1][4], reverse=True))
+
+        for pool in pools:
+            token0 = {
+                "address": pools[pool][0],
+                "symbol": pools[pool][1],
+            }
+            if token0 not in tokens:
+                tokens.append(token0)
+            token1 = {
+                "address": pools[pool][2],
+                "symbol": pools[pool][3],
+            }
+            if token1 not in tokens:
+                tokens.append(token1)
+                      
+    if blockchain == Chain.ETHEREUM:
+        dump(tokens, 'uniswap_v3', '_ethInfo.ts')
+
+
+protocol_data("ethereum", min_tvl_usd=1000000, min_volume_usd=1000000)
--- sdk/eth-sdk/config.ts
+++ sdk/eth-sdk/config.ts
@@ -116,6 +116,9 @@ export const contracts = {
       user_withdraw_manager: "0x9F0491B32DBce587c50c4C43AB303b06478193A7",
       ETHx: "0xA35b1B31Ce002FBF2058D22F30f95D405200A15b",
     },
+    uniswap_v3: {
+      positions_nft: "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
+    },
     dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
