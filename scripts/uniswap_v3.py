3fe87a0d2b156ee6fb077f0e109dcbcffcf87a50
nledo <103588782+nledo@users.noreply.github.com>
2024-07-04 16:24:28 -0300

    fix: Uniswap v3 new subgraph endpoint (#195)

--- scripts/uniswap_v3.py
+++ scripts/uniswap_v3.py
@@ -1,3 +1,4 @@
+import os
 import time
 # thegraph queries
 from gql import gql, Client
@@ -12,7 +13,9 @@ from lib.dump import dump
 def subgraph_query_all_pools(blockchain, min_tvl_usd=0, min_volume_usd=0):
 
     # Initialize subgraph
-    subgraph_url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+    the_graph_apikey = os.getenv('THE_GRAPH_APIKEY')
+    # Deprecated endpoint: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+    subgraph_url = f"https://gateway-arbitrum.network.thegraph.com/api/{the_graph_apikey}/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV"
     uniswapv3_transport = RequestsHTTPTransport(
         url=subgraph_url, verify=True, retries=3
     )
