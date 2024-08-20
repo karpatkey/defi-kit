86dd3d68e8debca179c7681aad7be93cbe157c30
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-08-20 20:35:20 +0200

    fix app build after adding arb1 sdk

--- app/server/sdk.ts
+++ app/server/sdk.ts
@@ -1,14 +1,13 @@
 import * as eth from "defi-kit/eth"
 import * as gno from "defi-kit/gno"
+import * as arb1 from "defi-kit/arb1"
 import { ActionName, Chain, NotFoundError, ProtocolActions } from "defi-kit"
 import { parseQuery } from "./parse"
 
 export const sdks = {
   eth,
   gno,
-  // bnb: 56,
-  // matic: 137,
-  // arb1: 42161,
+  arb1,
 } as const
 
 export type ChainPrefix = keyof typeof sdks
