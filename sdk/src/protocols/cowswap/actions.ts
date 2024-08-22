4289886cd9eaeeed427a73bbfc26da235408b5be
nledo <103588782+nledo@users.noreply.github.com>
2024-08-22 05:10:59 -0300

    fix: Cowswap Native Token handling (#225)
    Co-authored-by: Jan-Felix <jfschwarz@users.noreply.github.com>

--- sdk/src/protocols/cowswap/actions.ts
+++ sdk/src/protocols/cowswap/actions.ts
@@ -2,15 +2,15 @@ import { Chain } from "../../types"
 import { allow } from "zodiac-roles-sdk/kit"
 import { allowErc20Approve, oneOf } from "../../conditions"
 import { c, Permission } from "zodiac-roles-sdk"
-import { getOrderSignerAddressByChain, getWethAddressByChain } from "./utils"
+import { getWrappedNativeToken } from "./utils"
 
 const GPv2VaultRelayer = "0xC92E8bdf79f0507f65a392b0ab4667716BFE0110"
 const E_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
 
 export const swap = async (
   options: {
-    sell: (`0x${string}` | "ETH")[]
-    buy?: (`0x${string}` | "ETH")[]
+    sell: (`0x${string}` | "ETH" | "XDAI")[]
+    buy?: (`0x${string}` | "ETH" | "XDAI")[]
     feeAmountBp?: number
   },
   chain: Chain
@@ -36,18 +36,21 @@ export const swap = async (
     }
   }
 
-  const wethAddress = getWethAddressByChain(chain)
+  const wrappedNativeToken = getWrappedNativeToken(chain)
 
-  if ("ETH" in sell) {
+  if (sell.includes("ETH") || sell.includes("XDAI")) {
     permissions.push({
       ...allow.mainnet.weth.deposit({ send: true }),
-      targetAddress: wethAddress,
+      targetAddress: wrappedNativeToken,
     })
   }
 
-  const updatedSell = sell.map((item) => (item === "ETH" ? wethAddress : item))
+  const updatedSell = sell.map((item) =>
+    item === "ETH" || item === "XDAI" ? wrappedNativeToken : item
+  )
   const updatedBuy =
-    buy && buy.map((item) => (item === "ETH" ? E_ADDRESS : item))
+    buy &&
+    buy.map((item) => (item === "ETH" || item === "XDAI" ? E_ADDRESS : item))
 
   const orderStructScoping = {
     sellToken: oneOf(updatedSell),
@@ -55,27 +58,19 @@ export const swap = async (
     receiver: c.avatar,
   }
 
-  const orderSigner = getOrderSignerAddressByChain(chain)
-
   permissions.push(
     ...allowErc20Approve(updatedSell as `0x${string}`[], [GPv2VaultRelayer]),
 
-    {
-      ...allow.mainnet.cowswap.orderSigner.signOrder(
-        orderStructScoping,
-        undefined,
-        feeAmountBp !== undefined ? c.lte(feeAmountBp) : undefined,
-        { delegatecall: true }
-      ),
-      targetAddress: orderSigner,
-    },
+    allow.mainnet.cowswap.orderSigner.signOrder(
+      orderStructScoping,
+      undefined,
+      feeAmountBp !== undefined ? c.lte(feeAmountBp) : undefined,
+      { delegatecall: true }
+    ),
 
-    {
-      ...allow.mainnet.cowswap.orderSigner.unsignOrder(orderStructScoping, {
-        delegatecall: true,
-      }),
-      targetAddress: orderSigner,
-    }
+    allow.mainnet.cowswap.orderSigner.unsignOrder(orderStructScoping, {
+      delegatecall: true,
+    })
   )
 
   return permissions
--- sdk/src/protocols/cowswap/types.ts
+++
@@ -1,9 +0,0 @@
-import { contractAddressOverrides, contracts } from "../../../eth-sdk/config"
-
-export type WethAddress =
-  | typeof contracts.mainnet.weth
-  | (typeof contractAddressOverrides)[keyof typeof contractAddressOverrides]["weth"]
-
-export type OrderSignerAddress =
-  | typeof contracts.mainnet.cowswap.orderSigner
-  | typeof contractAddressOverrides.arbitrumOne.cowswap.orderSigner
