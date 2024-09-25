2a0508d2a6c31894ea9e0f15f228854e7065b443
nledo <nicolas@karpatkey.com>
2024-09-25 12:01:06 -0300

    Spark compatibility

--- sdk/src/protocols/spark/schema.ts
+++ sdk/src/protocols/spark/schema.ts
@@ -1,25 +1,49 @@
 import { z } from "zod"
 import ethTokens from "./_ethInfo"
+import gnoTokens from "./_gnoInfo"
 
-const zTokenDeposit = z.enum([
+const zEthTokenDeposit = z.enum([
   ...ethTokens.map((token) => token.symbol),
   "DSR_sDAI",
   "ETH",
   ...ethTokens.map((token) => token.token),
 ] as [string, string, ...string[]])
 
-const zTokenBorrow = z.enum([
+const zEthTokenBorrow = z.enum([
   ...ethTokens.map((token) => token.symbol),
   "ETH",
   ...ethTokens.map((token) => token.token),
 ] as [string, string, ...string[]])
 
+const zGnoTokenDeposit = z.enum([
+  ...gnoTokens.map((token) => token.symbol),
+  "DSR_sDAI",
+  "XDAI",
+  ...gnoTokens.map((token) => token.token),
+] as [string, string, ...string[]])
+
+const zGnoTokenBorrow = z.enum([
+  ...gnoTokens.map((token) => token.symbol),
+  "XDAI",
+  ...gnoTokens.map((token) => token.token),
+] as [string, string, ...string[]])
+
 export const eth = {
   deposit: z.object({
-    targets: zTokenDeposit.array(),
+    targets: zEthTokenDeposit.array(),
   }),
 
   borrow: z.object({
-    targets: zTokenBorrow.array(),
+    targets: zEthTokenBorrow.array(),
+  }),
+}
+
+export const gno = {
+  deposit: z.object({
+    targets: zGnoTokenDeposit.array(),
+  }),
+
+  borrow: z.object({
+    targets: zGnoTokenBorrow.array(),
   }),
 }
