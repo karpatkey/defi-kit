4df68fb5d68b9a211a25aee45161139cb73f0f11
coviccinelle <coviccinelle@gmail.com>
2024-10-30 14:21:51 +0100

    added arb1Token to types.ts

--- sdk/src/protocols/aave/v3/types.ts
+++ sdk/src/protocols/aave/v3/types.ts
@@ -1,7 +1,9 @@
 import ethTokens from "./_ethInfo"
 import gnoTokens from "./_gnoInfo"
+import arb1Tokens from "./_arb1Info"
 
 export type EthToken = (typeof ethTokens)[number]
 export type GnoToken = (typeof gnoTokens)[number]
+export type Arb1Token = (typeof arb1Tokens)[number]
 
-export type Token = EthToken | GnoToken
+export type Token = EthToken | GnoToken | Arb1Token
