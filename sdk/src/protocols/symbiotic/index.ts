76319f15a408e65de7b72737a5f44b13f16a420f
thao vit Ubuntu <coviccinelle@gmail.com>
2024-11-27 18:42:06 +0100

    wip symbiotic, stakeWise insp

--- sdk/src/protocols/symbiotic/index.ts
+++ sdk/src/protocols/symbiotic/index.ts
@@ -1,9 +1,8 @@
 import { allow } from "zodiac-roles-sdk/kit";
 import { Chain } from "../../types";
-import { deposit } from "./actions";
 
-const WSTETH= "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
-const DEFAULT_COLLATERAL = "0xC329400492c6ff2438472D4651Ad17389fCb843a"
+// const WSTETH= "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
+// const DEFAULT_COLLATERAL = "0xC329400492c6ff2438472D4651Ad17389fCb843a"
 
 export const eth = {
   deposit: async (
@@ -11,9 +10,7 @@ export const eth = {
       sell: (`0x${string}` | "ETH")[]
       buy?: (`0x${string}` | "ETH")[]
       feeAmountBp?: number
-    },
-  ) =>
-    deposit(options, Chain.eth),
+    },) => deposit(options, Chain.eth),
 }
     //deposit 1st pool
     // allow.mainnet.symbiotic.wstETHPool["deposit(address,uint256)"](),
