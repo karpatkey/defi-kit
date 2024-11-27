759245f54b0202f0151818a2a73ca7359ff68f74
thao vit Ubuntu <coviccinelle@gmail.com>
2024-11-27 15:08:40 +0100

    WIP: symbiotic: setup config add (proxy) for all Pools

--- sdk/eth-sdk/config.ts
+++ sdk/eth-sdk/config.ts
@@ -235,61 +235,26 @@ export const contracts = {
       routeProcessor32: "0x5550D13389bB70F45fCeF58f19f6b6e87F6e747d",
     },
     symbiotic: {
-      defaultCollateral: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6", //implementation
-
-      //wstETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6"//implemation for abis
-      wstETHPool: "0xC329400492c6ff2438472D4651Ad17389fCb843a",//proxy 2nd
+      defaultCollateral: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6", //implementation for all pools
       
-      WBTCPool: "0x971e5b5d4baa5607863f3748febf287c7bf82618",//proxy
-      //WBTCPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation 1st
-      
-      mETHPool: "0x475d3eb031d250070b63fa145f0fcfc5d97c304a", //proxy
-      //mETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6", //implementation
-
-      cbETHPool: "0xb26ff591f44b04e78de18f43b46f8b70c6676984",//proxy
-      //cbETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      rETHPool: "0x03bf48b8a1b37fbead1ecabcf15b98b924ffa5ac",//proxy
-      //rETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      wBETHPool: "0x422f5accc812c396600010f224b320a743695f85",//proxy
-      //wBETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      LBTCPool: "0x9c0823d3a1172f9ddf672d438dec79c39a64f448",//proxy
-      //LBTCPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      ENAPool: "0xe39b5f5638a209c1a6b6cdffe5d37f7ac99fcc84",//proxy
-      //ENAPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      sUSDePool: "0x19d0d8e6294b7a04a2733fe433444704b791939a",//proxy
-      //sUSDePool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      swETH: "0x38b86004842d3fa4596f0b7a0b53de90745ab654",//proxy
-      //swETH: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      sfrxETHPool: "0x5198cb44d7b2e993ebdda9cad3b9a0eaa32769d2",//proxy
-      //sfrxETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      ETHFIPool: "0x21dbba985eea6ba7f27534a72ccb292eba1d2c7c",//proxy
-      //ETHFIPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      LsETHPool: "0xb09a50acfff7d12b7d18adef3d1027bc149bad1c",//proxy
-      //LsETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      osETHPool: "0x52cb8a621610cc3ccf498a1981a8ae7ad6b8ab2a",//proxy
-      //osETHPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      FXSPool: "0x940750a267c64f3bbce31b948b67cd168f0843fa",//proxy
-      //FXSPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      ETHxPool: "0xbdea8e677f9f7c294a4556005c640ee505be6925",//proxy
-      //ETHxPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      tBTCPool: "0x0c969cec0729487d264716e55f232b404299032c",//proxy
-      //tBTCPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
-
-      SWELLPool: "0x544f45485418341c1a2b3a44404f12302277fffc",//proxy
-      //SWELLPool: "0xa301ea1E3CAB036AbE8fa70e5526A51Cb41799B6",//implementation
+      wstETethHPool: "0xC329400492c6ff2438472D4651Ad17389fCb843a",
+      WBTCPool: "0x971e5b5d4baa5607863f3748febf287c7bf82618",
+      mETHPool: "0x475d3eb031d250070b63fa145f0fcfc5d97c304a", 
+      cbETHPool: "0xb26ff591f44b04e78de18f43b46f8b70c6676984",
+      rETHPool: "0x03bf48b8a1b37fbead1ecabcf15b98b924ffa5ac",
+      wBETHPool: "0x422f5accc812c396600010f224b320a743695f85",
+      LBTCPool: "0x9c0823d3a1172f9ddf672d438dec79c39a64f448",
+      ENAPool: "0xe39b5f5638a209c1a6b6cdffe5d37f7ac99fcc84",
+      sUSDePool: "0x19d0d8e6294b7a04a2733fe433444704b791939a",
+      swETHPool: "0x38b86004842d3fa4596f0b7a0b53de90745ab654",
+      sfrxETHPool: "0x5198cb44d7b2e993ebdda9cad3b9a0eaa32769d2",
+      ETHFIPool: "0x21dbba985eea6ba7f27534a72ccb292eba1d2c7c",
+      LsETHPool: "0xb09a50acfff7d12b7d18adef3d1027bc149bad1c",
+      osETHPool: "0x52cb8a621610cc3ccf498a1981a8ae7ad6b8ab2a",
+      FXSPool: "0x940750a267c64f3bbce31b948b67cd168f0843fa",
+      ETHxPool: "0xbdea8e677f9f7c294a4556005c640ee505be6925",
+      tBTCPool: "0x0c969cec0729487d264716e55f232b404299032c",
+      SWELLPool: "0x544f45485418341c1a2b3a44404f12302277fffc",
     },
     theGraph: {
       staking: "0xF55041E37E12cD407ad00CE2910B8269B01263b9",
