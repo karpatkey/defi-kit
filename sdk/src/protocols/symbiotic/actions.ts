ed5f5f95b9c6d96fc9adf95f2f4cfe9f0876a67c
thao vit Ubuntu <coviccinelle@gmail.com>
2024-11-27 17:29:53 +0100

    wip symbiotic: actions and index

--- sdk/src/protocols/symbiotic/actions.ts
+++ sdk/src/protocols/symbiotic/actions.ts
@@ -1,4 +1,38 @@
+import { Permission } from "zodiac-roles-sdk/.";
 import { Chain } from "../../types";
 import { allow } from "zodiac-roles-sdk/kit";
 
-// export const deposit = async () => [
+export const deposit = async (
+    options: {
+        sell: (`0x${string}` | "ETH" )[]//
+        buy?: (`0x${string}` | "ETH" )[]
+        feeAmountBp?: number
+    },
+    chain: Chain
+) => {
+    const { sell, buy, feeAmountBp } = options
+    const permissions: Permission[] = []
+
+    if (sell.length === 0) {
+        throw new Error("`sell` must not be an empty array.")
+    }
+    if (buy && buy.length === 0) {
+        throw new Error(
+            "`buy` must not be an empty array. Pass `undefined` if you want to allow buying any token."
+        )
+    }
+    if (feeAmountBp !== undefined) {
+        if (
+            !Number.isInteger(feeAmountBp) ||
+            feeAmountBp < 0 ||
+            feeAmountBp > 10000
+        ) {
+            throw new Error("`feeAmountBp` must be an integer between 0 and 10000.")
+        }
+    }
+
+    // const wrappedNativeToken = getWrappedNativeToken(chain)
+
+
+    return permissions
+}
--- sdk/src/protocols/symbiotic/index.ts
+++ sdk/src/protocols/symbiotic/index.ts
@@ -1,15 +1,22 @@
 import { allow } from "zodiac-roles-sdk/kit";
 import { Chain } from "../../types";
+import { deposit } from "./actions";
 
 const WSTETH= "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 const DEFAULT_COLLATERAL = "0xC329400492c6ff2438472D4651Ad17389fCb843a"
 
 export const eth = {
-  deposit: async () => [
+  deposit: async (
+    options: {
+      sell: (`0x${string}` | "ETH")[]
+      buy?: (`0x${string}` | "ETH")[]
+      feeAmountBp?: number
+    },
+  ) =>
+    deposit(options, Chain.eth),
+}
     //deposit 1st pool
-    allow.mainnet.symbiotic.wstETHPool["deposit(address,uint256)"](),
-    allow.mainnet.lido.stEth.approve(WSTETH),
-    allow.mainnet.lido.wstEth.approve(DEFAULT_COLLATERAL),
-    allow.mainnet.lido.wstEth.wrap(),
-  ],
-} 
+    // allow.mainnet.symbiotic.wstETHPool["deposit(address,uint256)"](),
+    // allow.mainnet.lido.stEth.approve(WSTETH),
+    // allow.mainnet.lido.wstEth.approve(DEFAULT_COLLATERAL),
+    // allow.mainnet.lido.wstEth.wrap(),
