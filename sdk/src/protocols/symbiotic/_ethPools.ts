2058db72d88f87e82ab9941a8a9bafe71fe587b5
thao vit Ubuntu <coviccinelle@gmail.com>
2024-11-28 08:19:00 +0100

    _ethPools

--- sdk/src/protocols/symbiotic/_ethPools.ts
+++ sdk/src/protocols/symbiotic/_ethPools.ts
@@ -1,74 +1,76 @@
-export const ethPools = {
-  wstETHPoohl: {
-    name: "wstETHPoohl",
-    address: "0xC329400492c6ff2438472D4651Ad17389fCb843a",
+export default [
+  {
+    wstETHPoohl: {
+      name: "wstETHPoohl",
+      address: "0xC329400492c6ff2438472D4651Ad17389fCb843a",
+    },
+    WBTCPool: {
+      name: "WBTCPool",
+      address: "0x971e5b5d4baa5607863f3748febf287c7bf82618",
+    },
+    mETHPool: {
+      name: "mETHPool",
+      address: "0x475d3eb031d250070b63fa145f0fcfc5d97c304a",
+    },
+    cbETHPool: {
+      name: "cbETHPool",
+      address: "0xb26ff591f44b04e78de18f43b46f8b70c6676984",
+    },
+    rETHPool: {
+      name: "rETHPool",
+      address: "0x03bf48b8a1b37fbead1ecabcf15b98b924ffa5ac",
+    },
+    wBETHPool: {
+      name: "wBETHPool",
+      address: "0x422f5accc812c396600010f224b320a743695f85",
+    },
+    LBTCPool: {
+      name: "LBTCPool",
+      address: "0x9c0823d3a1172f9ddf672d438dec79c39a64f448",
+    },
+    ENAPool: {
+      name: "ENAPool",
+      address: "0xe39b5f5638a209c1a6b6cdffe5d37f7ac99fcc84",
+    },
+    sUSDePool: {
+      name: "sUSDePool",
+      address: "0x19d0d8e6294b7a04a2733fe433444704b791939a",
+    },
+    swETHPool: {
+      name: "swETHPool",
+      address: "0x38b86004842d3fa4596f0b7a0b53de90745ab654",
+    },
+    sfrxETHPool: {
+      name: "sfrxETHPool",
+      address: "0x5198cb44d7b2e993ebdda9cad3b9a0eaa32769d2",
+    },
+    ETHFIPool: {
+      name: "ETHFIPool",
+      address: "0x21dbba985eea6ba7f27534a72ccb292eba1d2c7c",
+    },
+    LsETHPool: {
+      name: "LsETHPool",
+      address: "0xb09a50acfff7d12b7d18adef3d1027bc149bad1c",
+    },
+    osETHPool: {
+      name: "osETHPool",
+      address: "0x52cb8a621610cc3ccf498a1981a8ae7ad6b8ab2a",
+    },
+    FXSPool: {
+      name: "FXSPool",
+      address: "0x940750a267c64f3bbce31b948b67cd168f0843fa",
+    },
+    ETHxPool: {
+      name: "ETHxPool",
+      address: "0xbdea8e677f9f7c294a4556005c640ee505be6925",
+    },
+    tBTCPool: {
+      name: "tBTCPool",
+      address: "0x0c969cec0729487d264716e55f232b404299032c",
+    },
+    SWELLPool: {
+      name: "SWELLPool",
+      address: "0x544f45485418341c1a2b3a44404f12302277fffc",
+    },
   },
-  WBTCPool: {
-    name: "WBTCPool",
-    address: "0x971e5b5d4baa5607863f3748febf287c7bf82618",
-  },
-  mETHPool: {
-    name: "mETHPool",
-    address: "0x475d3eb031d250070b63fa145f0fcfc5d97c304a",
-  },
-  cbETHPool: {
-    name: "cbETHPool",
-    address: "0xb26ff591f44b04e78de18f43b46f8b70c6676984",
-  },
-  rETHPool: {
-    name: "rETHPool",
-    address: "0x03bf48b8a1b37fbead1ecabcf15b98b924ffa5ac",
-  },
-  wBETHPool: {
-    name: "wBETHPool",
-    address: "0x422f5accc812c396600010f224b320a743695f85",
-  },
-  LBTCPool: {
-    name: "LBTCPool",
-    address: "0x9c0823d3a1172f9ddf672d438dec79c39a64f448",
-  },
-  ENAPool: {
-    name: "ENAPool",
-    address: "0xe39b5f5638a209c1a6b6cdffe5d37f7ac99fcc84",
-  },
-  sUSDePool: {
-    name: "sUSDePool",
-    address: "0x19d0d8e6294b7a04a2733fe433444704b791939a",
-  },
-  swETHPool: {
-    name: "swETHPool",
-    address: "0x38b86004842d3fa4596f0b7a0b53de90745ab654",
-  },
-  sfrxETHPool: {
-    name: "sfrxETHPool",
-    address: "0x5198cb44d7b2e993ebdda9cad3b9a0eaa32769d2",
-  },
-  ETHFIPool: {
-    name: "ETHFIPool",
-    address: "0x21dbba985eea6ba7f27534a72ccb292eba1d2c7c",
-  },
-  LsETHPool: {
-    name: "LsETHPool",
-    address: "0xb09a50acfff7d12b7d18adef3d1027bc149bad1c",
-  },
-  osETHPool: {
-    name: "osETHPool",
-    address: "0x52cb8a621610cc3ccf498a1981a8ae7ad6b8ab2a",
-  },
-  FXSPool: {
-    name: "FXSPool",
-    address: "0x940750a267c64f3bbce31b948b67cd168f0843fa",
-  },
-  ETHxPool: {
-    name: "ETHxPool",
-    address: "0xbdea8e677f9f7c294a4556005c640ee505be6925",
-  },
-  tBTCPool: {
-    name: "tBTCPool",
-    address: "0x0c969cec0729487d264716e55f232b404299032c",
-  },
-  SWELLPool: {
-    name: "SWELLPool",
-    address: "0x544f45485418341c1a2b3a44404f12302277fffc",
-  },
-}
+] as const
