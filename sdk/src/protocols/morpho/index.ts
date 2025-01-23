094dbaf790efc9e5eb6b3e23dd44560179ecdccf
coviccinelle <coviccinelle@gmail.com>
2025-01-23 11:11:59 +0100

    morpho Blue wip

--- sdk/src/protocols/morpho/index.ts
+++ sdk/src/protocols/morpho/index.ts
@@ -10,6 +10,14 @@ import { borrow } from "../maker/actions"
 
 const METAMORPHO_VAULT = "0x4881Ef0BF6d2365D3dd6499ccd7532bcdBCE0658" // Replace with the actual vault address
 // const ETHEREUM_BUNDLER = "0x4095F064B8d3c3548A3bebfd0Bbfd04750E30077" // EthereumBundlerV2
+const morphoBluePoolTest = {
+  loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+  irm: "0xbD60A6770b27E084E8617335ddE769241B0e71D8",
+  lltv: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+}
+
+const asset: ""
 
 const findPool = (nameOrAddress: string) => {
   const pools = _ethPools
@@ -41,29 +49,23 @@ export const eth = {
         {
           ...allow.mainnet.weth.approve(
             pool.address, //gtLRTcore vault
-            undefined,
+            undefined
           ),
           targetAddress: pool.asset.address,
         },
         {
-          ...allow.mainnet.morpho.metaMorpho.deposit(
-            undefined,
-            c.avatar,
-          ),
+          ...allow.mainnet.morpho.metaMorpho.deposit(undefined, c.avatar),
           targetAddress: pool.address,
         },
         {
-          ...allow.mainnet.morpho.metaMorpho.mint(
-            undefined,
-            c.avatar,
-          ),
+          ...allow.mainnet.morpho.metaMorpho.mint(undefined, c.avatar),
           targetAddress: pool.address,
         },
         {
           ...allow.mainnet.morpho.metaMorpho.withdraw(
             undefined,
             c.avatar,
-            c.avatar,
+            c.avatar
           ),
           targetAddress: pool.address,
         },
@@ -71,12 +73,10 @@ export const eth = {
           ...allow.mainnet.morpho.metaMorpho.redeem(
             undefined,
             c.avatar,
-            c.avatar,
+            c.avatar
           ),
           targetAddress: pool.address,
-        },
-
-       
+        }
       )
       return permissions
     })
@@ -91,15 +91,15 @@ export const eth = {
       const permissions: Permission[] = []
 
       permissions.push(
-       // *** Morpho Blue *** //
-
-       //Step0: appove: wstETH 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
-       //amount : 0.0 -> spender: 0x4095F064B8d3c3548A3bebfd0Bbfd04750E30077
-       //-> amount: 238442112376260655301
+        // *** Morpho Blue *** //
 
         //Step0: appove: wstETH 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
-       //amount : 0.0 -> spender: 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb
-       //-> amount: 238442112376260655301
+        //amount : 0.0 -> spender: 0x4095F064B8d3c3548A3bebfd0Bbfd04750E30077
+        //-> amount: 238442112376260655301
+
+        //Step0: appove: wstETH 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
+        //amount : 0.0 -> spender: 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb
+        //-> amount: 238442112376260655301
 
         //- Step1: supplyCollateral**
         //   loanToken: pool.asset.address,
@@ -107,32 +107,33 @@ export const eth = {
         //   oracle: contracts.mainnet.oracle.address,
         //   irm: contracts.mainnet.interestRateModel.address,
         //   lltv: pool.lltv,
-        //0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
+        //  0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
         // 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
         // 0xbD60A6770b27E084E8617335ddE769241B0e71D8,
         // 0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC
         // },
         // asset: pool.asset.address,
         // onBehalf: c.avatar, 0x0EFcCBb9E2C09Ea29551879bd9Da32362b32fc89
-        // {
-        //   ...allow.mainnet.morpho.morphoBlue.supplyCollateral(
-        //       {
-        //         //marketParams:
-        //       //   loanToken: pool.asset.address,
-        //       //   collateralToken: pool.address,
-        //       //   oracle: contracts.mainnet.oracle.address,
-        //       //   irm: contracts.mainnet.interestRateModel.address,
-        //       //   lltv: pool.lltv,
-        //       // },
-        //       // asset: pool.asset.address,
-        //       // onBehalf: c.avatar,
-        //     }
-        //   )
-        // },
-        
+        {
+          ...allow.mainnet.morpho.morphoBlue.supplyCollateral(
+            //marketParams:
+            morphoBluePoolTest,
+            // [
+            //   undefined, // -> loanToken
+            //   undefined, // -> collateralToken
+            //   undefined, //irm
+            //   undefined, //-> lltv
+            // ],
+            undefined, //asset
+            c.avatar, //onBehalf
+            "0x",
+          ),
+          targetAddress: pool.address,//TO CHANGE WITH NEW MORPHO BLUE POOL
+        },
+
         //Step 1.1: reallocateTo
         //contract: publicAllocator 0xfd32fA2ca22c76dD6E550706Ad913FC6CE91c75D
-        //vault: 0x2371e134e3455e0593363cBF89d3b6cf53740618
+        //vault: 0x2371e134e3455e0593363cBF89d3b6cf53740618 //(gtWETH) Gauntlet WETH Prime
         //withdrawls [tuple]:0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
         // 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
         // 0xc29B3Bc033640baE31ca53F8a0Eb892AdF68e663,
@@ -141,10 +142,15 @@ export const eth = {
         // 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
         // 0xbD60A6770b27E084E8617335ddE769241B0e71D8,
         // 0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC
+        {
+          ...allow.mainnet.morpho.publicAllocator.reallocateTo(
+          ),
+          targetAddress: pool.address,//TO CHANGE WITH NEW MORPHO BLUE POOL
+        },
 
         //Step2: borrow
         //contract: morpho 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb
-          //marketParams:0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,0xbD60A6770b27E084E8617335ddE769241B0e71D8,0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC
+        //marketParams:0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,0xbD60A6770b27E084E8617335ddE769241B0e71D8,0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC
 
         //   loanToken: pool.asset.address,
         //   collateralToken: pool.address,
@@ -155,13 +161,22 @@ export const eth = {
         // asset: 25944520971279508434
         // onBehalf: c.avatar, 0x0EFcCBb9E2C09Ea29551879bd9Da32362b32fc89
         // - receiver (address) 0x0EFcCBb9E2C09Ea29551879bd9Da32362b32fc89
-        // - data (optional)?
         // **Results:**
         // _0: The amount of assets borrowed.
         // _1: The amount of shares minted.
 
+        {
+          ...allow.mainnet.morpho.morphoBlue.borrow(
+            morphoBluePoolTest,
+            undefined,
+            undefined,
+            c.avatar,
+            c.avatar,
+          )
+        }
+
         //Step3: withdraw
-          //marketParams:
+        //marketParams:
         //   loanToken: pool.asset.address,
         //   collateralToken: pool.address,
         //   oracle: contracts.mainnet.oracle.address,
@@ -176,9 +191,8 @@ export const eth = {
         // _0: The amount of assets withdrawn.
         // _1: The amount of shares burned.
 
-
         //Step4: withdrawCollateral
-          //marketParams:
+        //marketParams:
         //   loanToken: pool.asset.address,
         //   collateralToken: pool.address,
         //   oracle: contracts.mainnet.oracle.address,
@@ -189,7 +203,6 @@ export const eth = {
         // onBehalf: c.avatar,
         // - receiver (address)
       )
-    },
-  )
-}
+    })
+  },
 }
