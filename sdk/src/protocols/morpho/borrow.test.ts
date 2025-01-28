8a611b7787137652245511b2a8ac5657f0bd104d
coviccinelle <coviccinelle@gmail.com>
2025-01-28 18:51:02 +0100

    wip morpho borrow test ok

--- sdk/src/protocols/morpho/borrow.test.ts
+++ sdk/src/protocols/morpho/borrow.test.ts
@@ -3,7 +3,7 @@ import { avatar } from "../../../test/wallets"
 import { contracts } from "../../../eth-sdk/config"
 import { applyPermissions, stealErc20 } from "../../../test/helpers"
 import { eth as kit } from "../../../test/kit"
-import { parseEther } from "ethers"
+import { AddressLike, BigNumberish, parseEther } from "ethers"
 
 // Test constants
 const MorphoBluePool = "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb" // gtLRTcore Vault address
@@ -48,121 +48,138 @@ describe("Morpho Blue borrow", () => {
             "0x"
           )
       ).not.toRevert()
-      const marketParams = [
-        "0xb8fc70e82bc5bb53e773626fcc6a23f7eefa036918d7ef216ecfb1950a94a85e",
-        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH9
-        "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
-        "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
-        "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
-        965000000000000000n,
-      ]
-      const marketParams2 = {
-        // marketId: "0xb8fc70e82bc5cc53e773626fbb6a23f7eefa036918d7ef216ecfb1950a94a85e",
-        // loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",//WETH9
+
+      const unauthorizedMarketParams = {
+        //dead
         loanToken: "0x000000000000000000000000000000000000dEaD",
         collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
         oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
         irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
         lltv: "965000000000000000",
       }
-    //   const withdrawalStruct = {
-    //     marketParams: marketParams,
-    //     amount: amount,
-    //     //   marketParams[0] = "0xDEADBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb"
-    //   }
 
       await expect(
         kit.asMember.morpho.morphoBlue
           .attach(MorphoBluePool)
-          .supplyCollateral(marketParams2, amount, avatar.address, "0x")
+          .supplyCollateral(unauthorizedMarketParams, amount, avatar.address, "0x")
       ).toRevert()
 
-      //----`
+      //----
 
-      const withdrawalStruct2 = [
-        marketParams,
-        amount,
-      ]
-      const supplyMarketParams = [
-        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
-        0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
-        0xbD60A6770b27E084E8617335ddE769241B0e71D8,
-        0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC
-      ]
+      const marketParams3 = {
+        loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH9
+        collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
+        oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
+        irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
+        lltv: "965000000000000000",
+      }
+      // await expect(
+      //   kit.asAvatar.morpho.publicAllocator
+      //     .attach(publicAllocator)
+      //     .reallocateTo(
+      //       "0x2371e134e3455e0593363cBF89d3b6cf53740618",
+      //       [{
+      //         marketParams: marketParams3,
+      //         amount: amount,
+      //       }],
+      //       {
+      //         loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      //         collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+      //         oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8",
+      //         irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+      //         lltv: "965000000000000000",
+      //       }
+      //     )
+      // ).not.toRevert()
 
-    //   await expect(
-    //     kit.asAvatar.morpho.publicAllocator
-    //       .attach(publicAllocator)
-    //       .reallocateTo(
-    //         "0x2371e134e3455e0593363cBF89d3b6cf53740618",
-    //         withdrawalStruct2,
-    //         supplyMarketParams,
-    //         // await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-    //         // await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-    //         // avatar.address,
-    //       )
-    //   ).not.toRevert()
       await expect(
         kit.asMember.morpho.morphoBlue
           .attach(MorphoBluePool)
           .borrow(
             await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-            amount,
-            shareAmount,
+            1,
+            0,
             avatar.address,
             avatar.address
           )
       ).not.toRevert()
+
+      await expect(
+        kit.asMember.morpho.morphoBlue
+          .attach(MorphoBluePool)
+          .borrow(
+            unauthorizedMarketParams,
+            1,
+            0,
+            avatar.address,
+            avatar.address
+          )
+      ).toRevert()
+
+
+      await expect(
+        kit.asMember.morpho.morphoBlue
+          .attach(MorphoBluePool)
+          .withdraw(
+            await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
+            amount,
+            0,
+            avatar.address,
+            avatar.address
+          )
+
+      ).not.toRevert()
+
+      // it("borrow WstETH", async () => {
+      //   const amount = BigInt(parseEther("2").toString())
+      //   const shareAmount = BigInt((amount / 2n).toString())
+
+      //   await stealErc20(underlying_wsteth, parseEther("10"), STEAL_ADDRESS)
+      //   await kit.asAvatar.weth
+      //     .attach(underlying_wsteth)
+      //     .approve(MorphoBluePool, amount)
+      //   //   await kit.asAvatar.morpho.publicAllocator.reallocateTo(
+      //   //     kit.asMember.morpho.morphoBlue,
+      //   //     await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
+      //   //     await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
+      //   //     // avatar.address,
+      //   //   )
+      //   await expect(
+      //     kit.asMember.morpho.morphoBlue
+      //       .attach(MorphoBluePool)
+      //       .borrow(
+      //         await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
+      //         amount,
+      //         shareAmount,
+      //         avatar.address,
+      //         avatar.address
+      //       )
+      //   ).not.toRevert()
+      //   const marketParams = [
+      //     "0xb8fc70e82bc5bb53e773626fcc6a23f7eefa036918d7ef216ecfb1950a94a85e",
+      //     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH9
+      //     "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
+      //     "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
+      //     "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
+      //     965000000000000000n,
+      //   ]
+      //   const marketParams2 = {
+      //     // marketId: "0xb8fc70e82bc5cc53e773626fbb6a23f7eefa036918d7ef216ecfb1950a94a85e",
+      //     // loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",//WETH9
+      //     loanToken: "0x000000000000000000000000000000000000dEaD",
+      //     collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
+      //     oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
+      //     irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
+      //     lltv: "965000000000000000",
+      //   }
+      //   //   marketParams[0] = "0xDEADBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb"
+
+      //   await expect(
+      //     kit.asMember.morpho.morphoBlue
+      //       .attach(MorphoBluePool)
+      //       .supplyCollateral(marketParams2, amount, avatar.address, "0x")
+      //   ).toRevert()
+      // })
     })
-    // it("borrow WstETH", async () => {
-    //   const amount = BigInt(parseEther("2").toString())
-    //   const shareAmount = BigInt((amount / 2n).toString())
-
-    //   await stealErc20(underlying_wsteth, parseEther("10"), STEAL_ADDRESS)
-    //   await kit.asAvatar.weth
-    //     .attach(underlying_wsteth)
-    //     .approve(MorphoBluePool, amount)
-    //   //   await kit.asAvatar.morpho.publicAllocator.reallocateTo(
-    //   //     kit.asMember.morpho.morphoBlue,
-    //   //     await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-    //   //     await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-    //   //     // avatar.address,
-    //   //   )
-    //   await expect(
-    //     kit.asMember.morpho.morphoBlue
-    //       .attach(MorphoBluePool)
-    //       .borrow(
-    //         await kit.asMember.morpho.morphoBlue.idToMarketParams(marketId),
-    //         amount,
-    //         shareAmount,
-    //         avatar.address,
-    //         avatar.address
-    //       )
-    //   ).not.toRevert()
-    //   const marketParams = [
-    //     "0xb8fc70e82bc5bb53e773626fcc6a23f7eefa036918d7ef216ecfb1950a94a85e",
-    //     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH9
-    //     "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
-    //     "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
-    //     "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
-    //     965000000000000000n,
-    //   ]
-    //   const marketParams2 = {
-    //     // marketId: "0xb8fc70e82bc5cc53e773626fbb6a23f7eefa036918d7ef216ecfb1950a94a85e",
-    //     // loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",//WETH9
-    //     loanToken: "0x000000000000000000000000000000000000dEaD",
-    //     collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
-    //     oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
-    //     irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
-    //     lltv: "965000000000000000",
-    //   }
-    //   //   marketParams[0] = "0xDEADBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb"
-
-    //   await expect(
-    //     kit.asMember.morpho.morphoBlue
-    //       .attach(MorphoBluePool)
-    //       .supplyCollateral(marketParams2, amount, avatar.address, "0x")
-    //   ).toRevert()
-    // })
   })
 })
