d37e8435d8445a4a30da52ea32a62ee95fb0f258
Zarathustra <98768518+sgzerbo@users.noreply.github.com>
2025-01-28 15:33:42 -0300

    Update jest.config.js timeout

--- sdk/jest.config.js
+++ sdk/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testTimeout: 45000,
+  testTimeout: 120000,
   globalSetup: "./test/global-setup.ts",
   setupFilesAfterEnv: ["./test/setup-after-env.ts"],
 }
