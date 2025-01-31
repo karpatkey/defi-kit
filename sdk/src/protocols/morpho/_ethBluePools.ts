7ef803a525cfb1c7dfa1084ba182df15446c1010
coviccinelle <coviccinelle@gmail.com>
2025-01-31 10:44:29 +0100

    wip morpho Blue eth pools

--- sdk/src/protocols/morpho/_ethBluePools.ts
+++ sdk/src/protocols/morpho/_ethBluePools.ts
@@ -1,10 +1,131 @@
 export default [
-    {
-        marketId: "0xb8fc70e82bc5bb53e773626fcc6a23f7eefa036918d7ef216ecfb1950a94a85e",
-        loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",//WETH9
-        collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",//WstETH
-        oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8",//MorphoChainlinkOracleV2
-        irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
-        lltv: "965000000000000000",
-    },
-] as const;
+  {
+    marketId:
+      "0xb8fc70e82bc5bb53e773626fcc6a23f7eefa036918d7ef216ecfb1950a94a85e",
+    loanToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", //WETH9
+    collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0", //WstETH
+    oracle: "0xbD60A6770b27E084E8617335ddE769241B0e71D8", //MorphoChainlinkOracleV2
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC", //AdaptiveCurveIrm
+    lltv: "965000000000000000",
+  },
+  {
+    collateralToken: "0x4c9EDD5852cd905f086C759E8383e09bff1E68B3",//USDe
+    loanToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F",//DAI
+    lltv: "770000000000000000",//77%
+    oracle: "0xaE4750d0813B5E37A51f7629beedd72AF1f9cA35",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xfd8493f09eb6203615221378d89f53fcd92ff4f7d62cca87eece9a2fff59e86f",
+  },
+  {
+    collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",//wstETH
+    loanToken: "0x7751E2F4b8ae93EF6B79d86419d42FE3295A4559",//wUSDL
+    lltv: "860000000000000000",//86%
+    oracle: "0xFb4b72b632CEC456321fEe556b7D4c9Af8D455e1",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xfd3e5c20340aeba93f78f7dc4657dc1e11b553c68c545acc836321a14b47e457",
+  },
+  {
+    collateralToken: "eBTC",//eBTC
+    loanToken: "LBTC",// LBTC
+    lltv: "915000000000000000",//91.5%
+    oracle: "0xD64A...09B6",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xfad6...18b6",
+  },
+  {
+    collateralToken: "WETH",//WETH
+    loanToken: "USDC",//USDC
+    lltv: "915000000000000000",//91.5%
+    oracle: "0xdC6f...7615",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xf9ac...b372",
+  },
+  {
+    collateralToken: "beraSTONE",//beraSTONE
+    loanToken: "WETH",//WETH
+    lltv: "915000000000000000",//91.5%
+    oracle: "0xaE95...dEE5",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xf78b...4fb6",
+  },
+  {
+    collateralToken: "LBTC",//LBTC
+    loanToken: "WBTC",//WBTC
+    lltv: "945000000000000000",//94.5%
+    oracle: "0xa981...4F80",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xf6a0...6549",
+  },
+  {
+    collateralToken: "USD3",//USD3
+    loanToken: "eUSD",//eUSD
+    lltv: "860000000000000000",//86%
+    oracle: "0xe35f...9e4C",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xf642...8068",
+  },
+  {
+    collateralToken: "rsETH",//rsETH
+    loanToken: "WETH",//WETH
+    lltv: "860000000000000000",//86%
+    oracle: "0x4236...8a4e",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xeeab...28b6",
+  },
+  {
+    collateralToken: "PAXG",//  PAXG
+    loanToken: "PYUSD",//PYUSD
+    lltv: "770000000000000000",//77%
+    oracle: "0xa051...FACE",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xeea9...a185",
+  },
+  {
+    collateralToken: "PT-ezETH-26DEC2024",//PT-ezETH-26DEC2024
+    loanToken: "USDA",//USDA
+    lltv: "860000000000000000",//86%
+    oracle: "0xfE63...B248",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xed9e...86e0",
+  },
+  {
+    collateralToken: "WOETH",//WOETH
+    loanToken: "WETH",//WETH
+    lltv: "860000000000000000",//86%
+    oracle: "0xb794...420e",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xea02...a12d",
+  },
+  {
+    collateralToken: "EIGEN",//EIGEN
+    loanToken: "USDC",//USDC
+    lltv: "770000000000000000",//77%
+    oracle: "0x9cfc...5770",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xe951...0d99",
+  },
+  {
+    collateralToken: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",//wstETH
+    loanToken: "USDT",//USDT
+    lltv: "860000000000000000",//86%
+    oracle: "0x95DB...6992",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xe7e9...87d2",
+  },
+  {
+    collateralToken: "tBTC",//tBTC
+    loanToken: "USDC",//USDC
+    lltv: "770000000000000000",//77%
+    oracle: "0x57bf...CA5b",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xe4cf...3f08",
+  },
+  {
+    collateralToken: "sUSDe",//sUSDe
+    loanToken: "0x6B175474E89094C44Da98b954EedeAC495271d0F",//DAI
+    lltv: "945000000000000000",//94.5%
+    oracle: "0x5D91...ae25",
+    irm: "0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC",
+    marketId: "0xe475...34d9",
+  },
+] as const
