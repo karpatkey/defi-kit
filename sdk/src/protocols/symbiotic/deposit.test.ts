bc19ddc9e3bfe634e24c24bb8df65ca8820a00ce
coviccinelle <coviccinelle@gmail.com>
2025-02-13 11:56:50 +0100

    fix symbiotic adapt with new chain + wallet system

--- sdk/src/protocols/symbiotic/deposit.test.ts
+++ sdk/src/protocols/symbiotic/deposit.test.ts
@@ -1,9 +1,10 @@
 import { eth } from "."
-import { avatar } from "../../../test/wallets"
+import { wallets } from "../../../test/wallets"
 import { applyPermissions, stealErc20 } from "../../../test/helpers"
 import { eth as kit } from "../../../test/kit"
 import _ethPools from "./_ethPools"
 import { parseEther } from "ethers"
+import { Chain } from "../../../src"
 
 const stealAddress = "0x3c22ec75ea5D745c78fc84762F7F1E6D82a2c5BF"
 const defaultCollateral = "0xC329400492c6ff2438472D4651Ad17389fCb843a" //wstETHPool
@@ -14,25 +15,28 @@ const stealAddress1 = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 describe("symbiotic", () => {
   describe("deposit action", () => {
     beforeAll(async () => {
-      await applyPermissions(await eth.deposit({ targets: ["wstETH"] }))
+      await applyPermissions(
+        Chain.eth,
+        await eth.deposit({ targets: ["wstETH"] })
+      )
     })
 
     it("deposit", async () => {
       const amount = parseEther("10")
-      await stealErc20(underlying, amount, stealAddress)
+      await stealErc20(Chain.eth, underlying, amount, stealAddress)
       await kit.asMember.weth
         .attach(underlying)
         .approve(defaultCollateral, amount)
       await expect(
         kit.asMember.symbiotic.defaultCollateral
           .attach(defaultCollateral)
-          ["deposit(address,uint256)"](avatar.address, amount)
+          ["deposit(address,uint256)"](wallets.avatar, amount)
       ).not.toRevert()
     })
 
     it("wrap stETH", async () => {
       const amount = parseEther("10")
-      await stealErc20(stETHToken, amount, stealAddress1)
+      await stealErc20(Chain.eth, stETHToken, amount, stealAddress1)
       await expect(
         kit.asMember.lido.stEth.approve(underlying, amount)
       ).not.toRevert()
@@ -41,19 +45,19 @@ describe("symbiotic", () => {
 
     it("withdraw", async () => {
       const amount = parseEther("10")
-      await stealErc20(underlying, amount, stealAddress)
+      await stealErc20(Chain.eth, underlying, amount, stealAddress)
       await kit.asMember.weth
         .attach(underlying)
         .approve(defaultCollateral, amount)
       await expect(
         kit.asMember.symbiotic.defaultCollateral
           .attach(defaultCollateral)
-          ["deposit(address,uint256)"](avatar.address, amount)
+          ["deposit(address,uint256)"](wallets.avatar, amount)
       ).not.toRevert()
       await expect(
         kit.asMember.symbiotic.defaultCollateral
           .attach(defaultCollateral)
-          .withdraw(avatar.address, amount)
+          .withdraw(wallets.avatar, amount)
       ).not.toRevert()
     })
   })
