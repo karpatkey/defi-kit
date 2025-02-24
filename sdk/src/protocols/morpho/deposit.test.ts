be2e9b4c981b5a9eb9ea113981cd087343febb15
coviccinelle <coviccinelle@gmail.com>
2025-02-24 11:28:40 +0100

    fix deposit test, replaced steal_address with a new one, bigger balance

--- sdk/src/protocols/morpho/deposit.test.ts
+++ sdk/src/protocols/morpho/deposit.test.ts
@@ -6,7 +6,7 @@ import { parseEther } from "ethers"
 import { Chain } from "../../../src"
 
 const METAMORPHO_VAULT = "0x4881Ef0BF6d2365D3dd6499ccd7532bcdBCE0658" // gtLRTcore Vault address
-const STEAL_ADDRESS = "0xD48573cDA0fed7144f2455c5270FFa16Be389d04" // Address to fund test WETH
+const STEAL_ADDRESS = "0xa1E2481a9CD0Cb0447EeB1cbc26F1b3fff3bec20" // Address to fund test WETH
 const underlying = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" // WETH
 
 describe("Morpho Protocol", () => {
