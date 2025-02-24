381c8d4695c79d723a2f7490e6b816aa17718867
coviccinelle <coviccinelle@gmail.com>
2025-02-24 11:18:47 +0100

    clean comments in schema and types morpho

--- sdk/src/protocols/morpho/types.ts
+++ sdk/src/protocols/morpho/types.ts
@@ -1,5 +1,3 @@
 import ethPools from "./_ethPools"
-// import _ethBluePools from "./_ethBluePools"
 
 export type EthPool = (typeof ethPools)[number]
-// export type EthBluePool = (typeof _ethBluePools)[number]
