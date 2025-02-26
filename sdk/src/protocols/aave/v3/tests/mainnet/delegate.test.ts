e7203c4a4b340bd8a3d5512e705b110867f4a1c5
Skwst <31214849+Skwst@users.noreply.github.com>
2025-02-26 16:47:58 +0200

    add stake and delegate tests on v3

---
+++ sdk/src/protocols/aave/v3/tests/mainnet/delegate.test.ts
@@ -0,0 +1,67 @@
+import { eth } from "../../index"
+import { wallets } from "../../../../../../test/wallets"
+import { applyPermissions } from "../../../../../../test/helpers"
+import { Status } from "../../../../../../test/types"
+import { eth as kit } from "../../../../../../test/kit"
+import { Chain } from "../../../../../index"
+
+const DELEGATEE = "0x849D52316331967b6fF1198e5E32A0eB168D039d"
+
+describe("aaveV3", () => {
+  describe("delegate", () => {
+    beforeAll(async () => {
+      await applyPermissions(
+        Chain.eth,
+        await eth.delegate({ targets: ["AAVE", "stkAAVE", "aEthAAVE"], delegatee: DELEGATEE })
+      )
+    })
+
+    it("allow delegation of AAVE to delegatee", async () => {
+      await expect(kit.asMember.aaveV2.aave.delegate(DELEGATEE)).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.aave.delegate(wallets.member)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+
+      await expect(
+        kit.asMember.aaveV2.aave.delegateByType(DELEGATEE, 0)
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.aave.delegateByType(wallets.member, 0)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+
+    it("allow delegation of stkAAVE to delegatee", async () => {
+      await expect(kit.asMember.aaveV2.stkAave.delegate(DELEGATEE)).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkAave.delegate(wallets.member)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+
+      await expect(
+        kit.asMember.aaveV2.stkAave.delegateByType(DELEGATEE, 0)
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkAave.delegateByType(wallets.member, 0)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+
+    it("allow delegation of aEthAAVE to delegatee", async () => {
+      await expect(kit.asMember.aaveV2.aEthAave.delegate(DELEGATEE)).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.aEthAave.delegate(wallets.member)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+
+      await expect(
+        kit.asMember.aaveV2.aEthAave.delegateByType(DELEGATEE, 0)
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.aEthAave.delegateByType(wallets.member, 0)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+  })
+})
---
+++ sdk/src/protocols/aave/v3/tests/mainnet/stake.test.ts
@@ -0,0 +1,168 @@
+import { eth } from "../../index"
+import { wallets } from "../../../../../../test/wallets"
+import {
+  applyPermissions,
+  stealErc20,
+  advanceTime,
+} from "../../../../../../test/helpers"
+import { contracts } from "../../../../../../eth-sdk/config"
+import { Status } from "../../../../../../test/types"
+import { eth as kit } from "../../../../../../test/kit"
+import { parseEther } from "ethers"
+import { Chain } from ".../../../src"
+
+describe("aaveV3", () => {
+  describe("stake", () => {
+    beforeAll(async () => {
+      await applyPermissions(
+        Chain.eth,
+        await eth.stake({ targets: ["AAVE", "ABPTV2", "GHO"] })
+      )
+    })
+
+    it("stake AAVE", async () => {
+      await stealErc20(
+        Chain.eth,
+        contracts.mainnet.aaveV2.aave,
+        parseEther("1"),
+        contracts.mainnet.balancer.vault
+      )
+
+      await expect(
+        kit.asMember.aaveV2.aave.approve(
+          contracts.mainnet.aaveV2.stkAave,
+          parseEther("1")
+        )
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkAave.stake(wallets.avatar, parseEther("1"))
+      ).not.toRevert()
+
+      await advanceTime(Chain.eth, 2)
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewards(wallets.avatar, 1)
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewardsAndStake(wallets.avatar, 1)
+      ).not.toRevert()
+
+      await expect(kit.asMember.aaveV2.stkAave.cooldown()).not.toRevert()
+
+      // The 20 days cooldown period must pass in order to withdraw the AAVE.
+      await advanceTime(Chain.eth, 1730000)
+      await expect(
+        kit.asMember.aaveV2.stkAave.redeem(wallets.avatar, 1)
+      ).not.toRevert()
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewardsAndStake(wallets.avatar, 1)
+      ).not.toRevert()
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewards(wallets.avatar, 1)
+      ).not.toRevert()
+    })
+
+    it("only allows staking and claiming AAVE from avatar", async () => {
+      await expect(
+        kit.asMember.aaveV2.stkAave.stake(wallets.member, parseEther("1"))
+      ).toBeForbidden(Status.ParameterNotAllowed)
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewardsAndStake(wallets.member, 1)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+      await expect(
+        kit.asMember.aaveV2.stkAave.claimRewards(wallets.member, 1)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+
+    it("stake ABPT", async () => {
+      await stealErc20(
+        Chain.eth,
+        contracts.mainnet.aaveV2.abptV2,
+        parseEther("1"),
+        contracts.mainnet.aaveV2.stkAbptV2
+      )
+
+      await expect(
+        kit.asMember.aaveV2.abptV2.approve(
+          contracts.mainnet.aaveV2.stkAbptV2,
+          parseEther("1")
+        )
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.stake(wallets.avatar, parseEther("1"))
+      ).not.toRevert()
+
+      await advanceTime(Chain.eth, 2)
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.claimRewards(wallets.avatar, 1)
+      ).toBeAllowed()
+
+      await expect(kit.asMember.aaveV2.stkAbptV2.cooldown()).not.toRevert()
+
+      // The 20 days cooldown period must pass in order to withdraw the AAVE.
+      await advanceTime(Chain.eth, 1730000)
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.redeem(wallets.avatar, 1)
+      ).not.toRevert()
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.claimRewards(wallets.avatar, 1)
+      ).toBeAllowed()
+    })
+
+    it("only allows staking ABPT and claiming AAVE from avatar", async () => {
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.stake(wallets.member, parseEther("1"))
+      ).toBeForbidden(Status.ParameterNotAllowed)
+      await expect(
+        kit.asMember.aaveV2.stkAbptV2.claimRewards(wallets.member, 1)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+
+    it("stake GHO", async () => {
+      await stealErc20(
+        Chain.eth,
+        contracts.mainnet.aaveV2.gho,
+        parseEther("1"),
+        contracts.mainnet.balancer.vault
+      )
+
+      await expect(
+        kit.asMember.aaveV2.gho.approve(
+          contracts.mainnet.aaveV2.stkGho,
+          parseEther("1")
+        )
+      ).not.toRevert()
+
+      await expect(
+        kit.asMember.aaveV2.stkGho.stake(wallets.avatar, parseEther("1"))
+      ).not.toRevert()
+
+      await advanceTime(Chain.eth, 2)
+      await expect(
+        kit.asMember.aaveV2.stkGho.claimRewards(wallets.avatar, 1)
+      ).toBeAllowed()
+
+      await expect(kit.asMember.aaveV2.stkGho.cooldown()).not.toRevert()
+
+      // The 20 days cooldown period must pass in order to withdraw the AAVE.
+      await advanceTime(Chain.eth, 1730000)
+      await expect(
+        kit.asMember.aaveV2.stkGho.redeem(wallets.avatar, 1)
+      ).not.toRevert()
+      await expect(
+        kit.asMember.aaveV2.stkGho.claimRewards(wallets.avatar, 1)
+      ).toBeAllowed()
+    })
+
+    it("only allows staking GHO and claiming AAVE from avatar", async () => {
+      await expect(
+        kit.asMember.aaveV2.stkGho.stake(wallets.member, parseEther("1"))
+      ).toBeForbidden(Status.ParameterNotAllowed)
+      await expect(
+        kit.asMember.aaveV2.stkGho.claimRewards(wallets.member, 1)
+      ).toBeForbidden(Status.ParameterNotAllowed)
+    })
+  })
+})
