a33365250283511ecd7b819550763bdfb78a99ae
nledo <nledo@users.noreply.github.com>
2025-05-30 00:30:54 +0000

    fix: protocol updates [nightly]

--- sdk/src/protocols/uniswap/v3/_arb1Info.ts
+++ sdk/src/protocols/uniswap/v3/_arb1Info.ts
@@ -54,10 +54,6 @@ export default [
     symbol: "CRV",
   },
   {
-    address: "0x9842989969687f7d249d01Cae1D2ff6b7b6b6D35",
-    symbol: "CRYPTO",
-  },
-  {
     address: "0x431402e8b9dE9aa016C743880e04E517074D8cEC",
     symbol: "HEGIC",
   },
@@ -66,12 +62,16 @@ export default [
     symbol: "ZRO",
   },
   {
-    address: "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
-    symbol: "tBTC",
-  },
-  {
     address: "0xba5DdD1f9d7F570dc94a51479a000E3BCE967196",
     symbol: "AAVE",
+  },
+  {
+    address: "0x9842989969687f7d249d01Cae1D2ff6b7b6b6D35",
+    symbol: "CRYPTO",
+  },
+  {
+    address: "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
+    symbol: "tBTC",
   },
   {
     address: "0x81314B07b2f8796a477f1c31c92241ee8D86301D",
@@ -88,10 +88,6 @@ export default [
   {
     address: "0x9623063377AD1B27544C965cCd7342f7EA7e88C7",
     symbol: "GRT",
-  },
-  {
-    address: "0xD8369C2EDA18dD6518eABb1F85BD60606dEb39Ec",
-    symbol: "WETH",
   },
   {
     address: "0x092bAaDB7DEf4C3981454dD9c0A0D7FF07bCFc86",
