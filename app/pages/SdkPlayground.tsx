5fc5963683ea55533a161bcc86edc0ee4e34b097
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-07-02 16:02:08 +0200

    load playground dynamically without ssr

--- app/pages/SdkPlayground.tsx
+++ app/pages/SdkPlayground.tsx
@@ -1,5 +1,18 @@
-import Playground from "@/components/Playground"
+import dynamic from "next/dynamic"
 import type { NextPage } from "next"
+
+const Playground = dynamic(() => import("@/components/Playground"), {
+  ssr: false,
+  loading: () => (
+    <div>
+      <div id="loader">
+        <p id="loading-message" role="status">
+          Loading playground...
+        </p>
+      </div>
+    </div>
+  ),
+})
 
 const SdkPlayground: NextPage = () => {
   return (
