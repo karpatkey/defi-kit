ebc6cfcb20c3e34fed4370a42bc8d04f64af2740
nledo <nledo@users.noreply.github.com>
2025-08-25 00:39:14 +0000

    fix: protocol updates [nightly]

--- sdk/src/protocols/uniswap/v3/_oethInfo.ts
+++ sdk/src/protocols/uniswap/v3/_oethInfo.ts
@@ -10,7 +10,7 @@ export default [
     symbol: "PIKA",
   },
   {
-    address: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
+    address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
     symbol: "USDC",
   },
   {
@@ -18,19 +18,23 @@ export default [
     symbol: "OP",
   },
   {
-    address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
-    symbol: "USDC",
-  },
-  {
     address: "0x68f180fcCe6836688e9084f035309E29Bf0A2095",
     symbol: "WBTC",
   },
   {
-    address: "0xdC6fF44d5d932Cbd77B52E5612Ba0529DC6226F1",
-    symbol: "WLD",
+    address: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
+    symbol: "USDC",
   },
   {
     address: "0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb",
     symbol: "wstETH",
+  },
+  {
+    address: "0x139052115F8B1773cF7DcBA6a553F922a2E54F69",
+    symbol: "(=ↀωↀ=)",
+  },
+  {
+    address: "0xeF4461891DfB3AC8572cCf7C794664A8DD927945",
+    symbol: "WCT",
   },
 ] as const
